de5aa8ddd3c5117b83bfd6b8f701630ce7d7c52d
Kowallukasz01 <Kowallukasz01@gmail.com>
2018-10-29 21:15:12 +0100

    Added validation and additional view models

---
+++ SpodIglyMVC/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿
+using SpodIglyMVC.ViewModels;
+using System.Web.Mvc;
+
+namespace SpodIglyMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        // returnUrl - app zapisze strone na ktora chcial sie dostac uzytkownik i po udanym logowaniu przekieruje go na nia
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        
+        // akcja bedzie wywolana tylko i wylacznie po POST
+        [HttpPost]
+        public ActionResult Login(LoginViewModels viewModel, string returnUrl)
+        {
+            if(!ModelState.IsValid)
+            {
+                // jezeli model bedzi  e zawierac bledy to poinformuje o tym uzytkownika i zwroci mu to co wpisal
+                return View(viewModel);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            else
+            {
+                return RedirectToAction("index", "Home"); 
+            }
+        }
+    }
+}
---
+++ SpodIglyMVC/ViewModels/AccountViewModels.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace SpodIglyMVC.ViewModels
+{
+    public class LoginViewModels
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamietaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required()]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
